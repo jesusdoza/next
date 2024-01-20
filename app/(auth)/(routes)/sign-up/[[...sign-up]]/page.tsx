2ef24cfdc8bea607905ff2ecf810ff3fc21cce07
jesus doza <jesus.doza@gmail.com>
2024-01-20 15:40:38 -0600

    add clerk provider and signup button

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -2,8 +2,7 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div className="">
-      this is a protected route
+    <div className="h-screen">
       <UserButton afterSignOutUrl="/" />
     </div>
   );
